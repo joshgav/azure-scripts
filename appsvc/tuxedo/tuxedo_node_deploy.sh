8b318cac0cc08acba04c795944abd639da4ec9d8
Josh Gavant <josh.gavant@outlook.com>
2016-11-07 16:46:34 -0800

    clean up appsvc scripts

--- appsvc/tuxedo/tuxedo_node_deploy.sh
+++ appsvc/tuxedo/tuxedo_node_deploy.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env sh
 
 export AZURE_RG_NAME="rg-tuxedo-node"
-export AZURE_RG_DEPLOYMENT_NAME="rg-instance-tuxedo-node-01"
+export AZURE_RG_DEPLOYMENT_NAME="rg-tuxedo-node-01"
 
 azure group create \
     --name ${AZURE_RG_NAME} \
     --location westus \
     --deployment-name ${AZURE_RG_DEPLOYMENT_NAME} \
-    --template-file "./arm/tuxedo_node.json" \
-    --parameters-file "./arm/tuxedo_node.params.json"
+    --template-file "./tuxedo_node.json" \
+    --parameters-file "./tuxedo_node.params.json"
 
-# azure webapp config set $AZURE_RG_NAME 'joshgav-tuxedo-app-01' --nodeversion 6.6.0
+# azure webapp config set $AZURE_RG_NAME 'joshgav-tuxedo-node-app-01' --nodeversion 6.6.0
 
 # azure group deployment operation list $AZURE_RG_NAME $AZURE_RG_DEPLOYMENT_NAME
 
