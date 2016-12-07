ee5196a29a3107f350a56203ac39938046a119e6
Josh Gavant <josh.gavant@outlook.com>
2016-12-07 11:29:49 -0800

    update appsvc script

--- appsvc/no-arm-deploy.sh
+++ appsvc/no-arm-deploy.sh
@@ -6,40 +6,43 @@ export AZURE_APPSVC_PLAN_NAME="joshgav-tuxedo-node-farm"
 export AZURE_APPSVC_WEB_NAME="joshgav-tuxedo-node-web"
 
 export AZURE_NODE_VERSION="6.6.0"
-export AZURE_NODE_MAIN="server.js"
+export AZURE_NODE_MAIN="index.js"
 
 export GIT_REPO_URL="https://github.com/joshgav/node-scratch"
-export GIT_BRANCH="az-appsvc"
+export GIT_BRANCH="master"
 
 az resource group create \
-    --name ${AZURE_RG_NAME} \
-    --location ${AZURE_RG_LOCATION} \
+  --name ${AZURE_RG_NAME} \
+  --location ${AZURE_RG_LOCATION} \
 
 az appservice plan create \
-    --sku S1 \
-    --resource-group ${AZURE_RG_NAME} \
-    --name ${AZURE_APPSVC_PLAN_NAME} \
-    --location ${AZURE_RG_LOCATION} \
-    --is-linux
+  --sku S1 \
+  --resource-group ${AZURE_RG_NAME} \
+  --name ${AZURE_APPSVC_PLAN_NAME} \
+  --location ${AZURE_RG_LOCATION} \
+  --is-linux
 
 az appservice web create \
-    --resource-group ${AZURE_RG_NAME} \
-    --plan ${AZURE_APPSVC_PLAN_NAME} \
-    --name ${AZURE_APPSVC_WEB_NAME}
+  --resource-group ${AZURE_RG_NAME} \
+  --plan ${AZURE_APPSVC_PLAN_NAME} \
+  --name ${AZURE_APPSVC_WEB_NAME}
 
 az appservice web config update \
-    --resource-group ${AZURE_RG_NAME} \
-    --name ${AZURE_APPSVC_WEB_NAME} \
-    --node-version ${AZURE_NODE_VERSION} \
-    --startup-file ${AZURE_NODE_MAIN}
+  --resource-group ${AZURE_RG_NAME} \
+  --name ${AZURE_APPSVC_WEB_NAME} \
+  --node-version ${AZURE_NODE_VERSION} \
+  --startup-file ${AZURE_NODE_MAIN}
 
 az appservice web source-control config \
-    --resource-group ${AZURE_RG_NAME} \
-    --name ${AZURE_APPSVC_WEB_NAME} \
-    --repo-url ${GIT_REPO_URL} \
-    --branch ${GIT_BRANCH}
+  --resource-group ${AZURE_RG_NAME} \
+  --name ${AZURE_APPSVC_WEB_NAME} \
+  --repo-url ${GIT_REPO_URL} \
+  --branch ${GIT_BRANCH}
 
+# must force first sync manually
 az appservice web source-control sync \
-    --resource-group ${AZURE_RG_NAME} \
-    --name ${AZURE_APPSVC_WEB_NAME}
+  --resource-group ${AZURE_RG_NAME} \
+  --name ${AZURE_APPSVC_WEB_NAME}
+
+# note: must stop and start app in portal once to work :(
 
