a60b17c66b85b074fdf134c550b433e786df6222
Josh Gavant <josh.gavant@outlook.com>
2017-04-27 11:33:18 -0700

    update webapp script

--- appsvc/webapp-deploy.sh
+++ appsvc/webapp-deploy.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env sh
 
+# expect these env vars in .env:
+# APPINSIGHTS_INSTRUMENTATIONKEY
+# GITHUB_TOKEN
+set -a
+eval $(cat ./.env)
+set +a
+
 export AZURE_RG_NAME="rg-appsvc-node3"
 export AZURE_RG_LOCATION="westus"
 export AZURE_APPSVC_PLAN_NAME="joshgav-appsvc-node3-plan"
 export AZURE_APPSVC_WEB_NAME="joshgav-appsvc-node3-web"
 
-export AZURE_NODE_VERSION="6.6.0"
+export AZURE_NODE_VERSION="NODE|6.9"
 export AZURE_NODE_MAIN="bootstrap.js"
 
 export GIT_REPO_URL="https://github.com/joshgav/node-scratch"
@@ -17,6 +24,7 @@ az group create \
 
 az appservice plan create \
   --sku S1 \
+  --number-of-workers 2 \
   --resource-group ${AZURE_RG_NAME} \
   --name ${AZURE_APPSVC_PLAN_NAME} \
   --location ${AZURE_RG_LOCATION} \
@@ -30,19 +38,17 @@ az appservice web create \
 az appservice web config update \
   --resource-group ${AZURE_RG_NAME} \
   --name ${AZURE_APPSVC_WEB_NAME} \
-  --node-version ${AZURE_NODE_VERSION} \
+  --linux-fx-version ${AZURE_NODE_VERSION} \
   --startup-file ${AZURE_NODE_MAIN}
+
+az appservice web config appsettings update \
+  --resource-group ${AZURE_RG_NAME} \
+  --name ${AZURE_APPSVC_WEB_NAME} \
+  --settings APPINSIGHTS_INSTRUMENTATIONKEY=$APPINSIGHTS_INSTRUMENTATIONKEY
 
 az appservice web source-control config \
   --resource-group ${AZURE_RG_NAME} \
   --name ${AZURE_APPSVC_WEB_NAME} \
   --repo-url ${GIT_REPO_URL} \
   --branch ${GIT_BRANCH}
-
-# must force first sync manually
-az appservice web source-control sync \
-  --resource-group ${AZURE_RG_NAME} \
-  --name ${AZURE_APPSVC_WEB_NAME}
-
-# note: must stop and start app in portal once to work :(
 
